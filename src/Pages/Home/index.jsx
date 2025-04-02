e6e4153efd8772fcc3e2243b4e51f0bb18e5d4bf
Akbarshoh <ismatov2009akb@gmail.com>
2025-04-02 22:46:52 +0500

    responsive section5

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,11 +3,15 @@ import React from "react";
 import Navbar from "../../Layoutes/Navbar";
 import Header from "./Header";
 
+import Section5 from "../Section5";
+
 const Home = () => {
   return (
     <>
       <Navbar />
       <Header />
+
+      <Section5 />
     </>
   );
 };
---
+++ src/Pages/Section5/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Section5 = () => {
+  return (
+    <div className={styles.section}>
+      <div className={styles.container}>
+        <p>Ready to have a decorated lifestyle?</p>
+        <button>Start Shopping</button>
+      </div>
+    </div>
+  );
+};
+
+export default Section5;
